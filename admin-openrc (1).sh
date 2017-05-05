2a8d9f8a9d826f61ab91c373f0f354609b1cdc2b
aymen <aymen.mansour@hotmail.com>
2017-05-06 01:45:45 +0200

    ok

--- admin-openrc (1).sh
+++ admin-openrc (1).sh
@@ -7,29 +7,29 @@
 # Storage, and Networking (code-named nova, glance, keystone, swift,
 # cinder, and neutron).
 #
-# *NOTE*: Using the 2.0 *Identity API* does not necessarily mean any other
-# OpenStack API is version 2.0. For example, your cloud provider may implement
+# *NOTE*: Using the 3 *Identity API* does not necessarily mean any other
+# OpenStack API is version 3. For example, your cloud provider may implement
 # Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
 # only for the Identity API served through keystone.
-export OS_AUTH_URL=http://10.0.2.15/identity
+export OS_AUTH_URL=http://10.0.2.15/identity/v3
 
-# With the addition of Keystone we have standardized on the term **tenant**
+# With the addition of Keystone we have standardized on the term **project**
 # as the entity that owns the resources.
-export OS_TENANT_ID=ba4dc4f301ef44cba382ccea887b28ce
-export OS_TENANT_NAME="admin"
+export OS_PROJECT_ID=ba4dc4f301ef44cba382ccea887b28ce
+export OS_PROJECT_NAME="admin"
+export OS_USER_DOMAIN_NAME="Default"
+if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
 
-# unsetting v3 items in case set
-unset OS_PROJECT_ID
-unset OS_PROJECT_NAME
-unset OS_USER_DOMAIN_NAME
-unset OS_INTERFACE
+# unset v2.0 items in case set
+unset OS_TENANT_ID
+unset OS_TENANT_NAME
 
 # In addition to the owning entity (tenant), OpenStack stores the entity
 # performing the action as the **user**.
 export OS_USERNAME="admin"
 
 # With Keystone you pass the keystone password.
-echo "Please enter your OpenStack Password for project $OS_TENANT_NAME as user $OS_USERNAME: "
+echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
 read -sr OS_PASSWORD_INPUT
 export OS_PASSWORD=$OS_PASSWORD_INPUT
 
@@ -39,5 +39,5 @@ export OS_REGION_NAME="RegionOne"
 # Don't leave a blank variable, unset it if it was empty
 if [ -z "$OS_REGION_NAME" ]; then unset OS_REGION_NAME; fi
 
-export OS_ENDPOINT_TYPE=publicURL
-export OS_IDENTITY_API_VERSION=2
+export OS_INTERFACE=public
+export OS_IDENTITY_API_VERSION=3
